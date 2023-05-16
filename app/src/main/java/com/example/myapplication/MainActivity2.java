cd6b1f2152ed99ffc31e201ad73027e824ae8258
vishal <vishalgupta64akad@gmail.com>
2023-05-16 16:08:03 +0530

    fifth commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MediaPlayer mp=MediaPlayer.create(MainActivity2.this, R.raw.stranger);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+
+
+    }
+    public void openMainActivity(){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
